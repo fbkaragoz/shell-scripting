f33e7a956d62cbccb2736faa25e0cae5cdae919a
rekurrenzk <Fatihburak@protonmail.com>
2024-09-17 15:52:23 +0300

    nothing fancy. just started learning rust

---
+++ experimental/unit_exp.rs
@@ -0,0 +1,19 @@
+//learning rust guys.
+    //nothing to see here.
+fn greet(name: &str) {
+    println!("hello, {}", name);
+}
+
+fn main() {
+    let x: i32 =5;
+    let y=10;
+    println!("x is {} and y is {}", x, y);
+
+    greet("rust learner!");
+
+    println!("you did it!! this is your first function in rust");
+
+}
+
+
+
